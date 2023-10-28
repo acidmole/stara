ffe7b6a316f6affb86883d4e1c07cb88861d47c8
Heikki Venhola <hede@heikkivenhola.fi>
2023-10-28 20:48:55 +0300

    fix bugs

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -1,9 +1,9 @@
-#require 'discordrb'
+require 'discordrb'
 
-#Bot = Discordrb::Bot.new(
-# token: ENV['DISCORD_TOKEN'],
-#  client_id: ENV['DISCORD_CLIENT_ID']
-#)
+Bot = Discordrb::Bot.new(
+ token: ENV['DISCORD_TOKEN'],
+  client_id: ENV['DISCORD_CLIENT_ID']
+)
 
-#Dir["#{Rails.root}/app/commands/*.rb"].each { |file| require file }
-#Bot.run(true)
+Dir["#{Rails.root}/app/commands/*.rb"].each { |file| require file }
+Bot.run(true)
